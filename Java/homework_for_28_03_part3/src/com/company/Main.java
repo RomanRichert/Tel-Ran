27b82f540c4871ea8830d9b79b66d043de802f69
Roman Richert <roman_richert@vk.com>
2022-03-26 21:49:38 +0100

    Homework for 28.03.2022

---
+++ Java/homework_for_28_03_part3/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter N: ");
+        int n1 = sc.nextInt();
+        int n = 1;
+        int sum=0;
+        while (n<=n1) {
+            sum = sum +n*n;
+            n++;
+        }
+        System.out.println(sum);
+    }
+}
