acfb8d4de0da49d738579047bcaeac5349a945eb
Roman Richert <roman_richert@vk.com>
2022-04-03 15:38:20 +0200

    Домашка на 5 апреля. 1 задача. Работает только с этой строкой.

---
+++ Java/homework_for_05_04_task1/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Implement the method that takes String and print the first word of the String (all chars before the first space)
+        //(“Пример строки”) ->”Пример”
+        System.out.println(example("Пример строки"));
+    }
+    public static String example (String str) {
+        String result = ""+str.charAt(0)+str.charAt(1)+str.charAt(2)+str.charAt(3)+str.charAt(4)+str.charAt(5);
+        return result;
+    }
+}
