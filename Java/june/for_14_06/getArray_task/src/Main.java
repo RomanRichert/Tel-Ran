59affba21bc514e5acb41dcaa68d00ddd32a38d2
Roman Richert <roman_richert@vk.com>
2022-06-13 20:52:23 +0200

    Добавил описание

--- Java/june/for_14_06/getArray_task/src/Main.java
+++ Java/june/for_14_06/getArray_task/src/Main.java
@@ -3,6 +3,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //Imagine in our program we process an array (for example, an array of integers, or an array of Employee if you want).
+        // In production environment we read this array from User (use Scanner),
+        // in test environment we get it as random array or use an array defined in the code.
+        // So, you need to implement a method getArray() witch as argument takes the way,
+        // how the array will be created and as a result returns the array. Implement this program (hint: use knowledge about interface)
+
         Scanner sc = new Scanner(System.in);
         ArrayDistributer giveMeAnArray = new ArrayDistributer();
 
