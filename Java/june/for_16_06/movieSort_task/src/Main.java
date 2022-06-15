34af2f421d52b440ab9a8c8ced1574a86981c882
Roman Richert <roman_richert@vk.com>
2022-06-15 21:10:15 +0200

    Домашка с сортировкой фильмов, и другие мелкие изменения

--- Java/june/for_16_06/movieSort_task/src/Main.java
+++ Java/june/for_16_06/movieSort_task/src/Main.java
@@ -9,16 +9,16 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         List<Movie> movieList = new ArrayList<>();
-            movieList.add(new Movie("The Shawshank Redemption                         ", "Drama                   ", 9.3, 1994));
-            movieList.add(new Movie("The Godfather                                    ", "Crime, drama            ", 9.2, 1972));
-            movieList.add(new Movie("The Dark Knight                                  ", "Action, crime, drama    ", 9.0, 2008));
-            movieList.add(new Movie("The Godfather Part II                            ", "Crime, drama            ", 9.0, 1974));
-            movieList.add(new Movie("12 Angry Men                                     ", "Crime, Drama            ", 9.0, 1957));
-            movieList.add(new Movie("Schindler's List                                 ", "Biography,drama, history", 9.0, 1993));
-            movieList.add(new Movie("The Lord of the Rings: The Return of the King    ", "Action, adventure, drama", 9.0, 2003));
-            movieList.add(new Movie("Pulp Fiction                                     ", "Crime, drama            ", 8.9, 1994));
-            movieList.add(new Movie("The Lord of the Rings: The Fellowship of the Ring", "Action, adventure, drama", 8.8, 2001));
-            movieList.add(new Movie("Il buono, il brutto, il cattivo                  ", "Adventure, western      ", 8.8, 1966));
+            movieList.add(new Movie("The Shawshank Redemption                         ", "Drama                    ", 9.3, 1994));
+            movieList.add(new Movie("The Godfather                                    ", "Crime, drama             ", 9.2, 1972));
+            movieList.add(new Movie("The Dark Knight                                  ", "Action, crime, drama     ", 9.0, 2008));
+            movieList.add(new Movie("The Godfather Part II                            ", "Crime, drama             ", 9.0, 1974));
+            movieList.add(new Movie("12 Angry Men                                     ", "Crime, drama             ", 9.0, 1957));
+            movieList.add(new Movie("Schindler's List                                 ", "Biography, drama, history", 9.0, 1993));
+            movieList.add(new Movie("The Lord of the Rings: The Return of the King    ", "Action, adventure, drama ", 9.0, 2003));
+            movieList.add(new Movie("Pulp Fiction                                     ", "Crime, drama             ", 8.9, 1994));
+            movieList.add(new Movie("The Lord of the Rings: The Fellowship of the Ring", "Action, adventure, drama ", 8.8, 2001));
+            movieList.add(new Movie("Il buono, il brutto, il cattivo                  ", "Adventure, western       ", 8.8, 1966));
 
         Movie[] movieArray = movieListToMovieArray(movieList);
         HowToSort newSort = new HowToSort();
@@ -37,6 +37,7 @@ public class Main {
     }
 
     public static void print(Movie[] movieArr){
+        System.out.println("                     Name                    Release year "+"          Genre              "+"Rating"+" ");
         for (Movie u:movieArr ) {
             System.out.println(u);
         }
